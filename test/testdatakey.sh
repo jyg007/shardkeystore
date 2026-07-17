ea7d90cbce389ea396f52262e550657a1ea20770
root <jyg007@gmail.com>
2026-07-17 05:47:40 -0400

    fix https

--- test/testdatakey.sh
+++ test/testdatakey.sh
@@ -1,6 +1,6 @@
 # Generate key
 RESPONSE=$(curl -sk --cert client.crt --key client.key --cacert ca.crt \
-            -X POST http://localhost:4433/generateDataKey \
+            -X POST https://localhost:4433/generateDataKey \
             -H "Content-Type: application/json" \
             -d '{}')
 echo $RESPONSE
@@ -8,7 +8,7 @@ ENCRYPTED=$(echo $RESPONSE | jq -r '.encryptedKey')
 
 # Decrypt key
 curl -sk --cert client.crt --key client.key --cacert ca.crt \
-     -X POST http://localhost:4433/decryptDataKey \
+     -X POST https://localhost:4433/decryptDataKey \
      -H "Content-Type: application/json" \
      -d "{\"encryptedKey\":\"$ENCRYPTED\"}"
 
